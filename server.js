cf91b961c25efb2150daf2d9c7beec2e3c7a381b
Ricardo Soto-Fabela <111827597+Ricardo2450@users.noreply.github.com>
2022-11-21 15:41:28 -0600

    Initial commit

---
+++ server.js
@@ -0,0 +1,18 @@
+'use strict';
+
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+app.use(cors());
+
+const PORT = process.env.PORT || 3001;
+
+app.get('/test', (request, response) => {
+
+  response.send('test request received')
+
+})
+
+app.listen(PORT, () => console.log(`listening on ${PORT}`));
